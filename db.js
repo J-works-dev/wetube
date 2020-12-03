b64c9ddbce883729cb4302ad1f9a9bcd1025e3d8
J-works-dev <sangjoon.jeremy.lee@gmail.com>
2020-12-03 20:17:19 +0800

    valueless git(test)

--- db.js
+++ db.js
@@ -1,15 +1,67 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-mongoose.connect("mongodb://localhost:27017/wetube",
+// mongoose.connect("mongodb://localhost:27017/wetube",
+//     {
+//     useNewUrlParser: true,
+//     useFindAndModify: false
+//     }
+//     );
+
+// const db = mongoose.connection;
+
+// const handleOpen = () => console.log("Connected to DB");
+// const handleError = (error) => console.log(`Error on DB Cnnection on DB Connection:${error}`);
+// db.once("open", handleOpen);
+// db.on("error");
+
+
+export const videos = [
     {
-    useNewUrlParser: true,
-    useFindAndModify: false
+      id: 324393,
+      title: "Video awesome",
+      description: "This is something I love",
+      views: 24,
+      videoFile: "https://archive.org/details/BigBuckBunny_124",
+      creator: {
+        id: 121212,
+        name: "Nicolas",
+        email: "nico@las.com"
+      }
+    },
+    {
+      id: 1212121,
+      title: "Video super",
+      description: "This is something I love",
+      views: 24,
+      videoFile: "https://archive.org/details/BigBuckBunny_124",
+      creator: {
+        id: 121212,
+        name: "Nicolas",
+        email: "nico@las.com"
+      }
+    },
+    {
+      id: 55555,
+      title: "Video nice",
+      description: "This is something I love",
+      views: 24,
+      videoFile: "https://archive.org/details/BigBuckBunny_124",
+      creator: {
+        id: 121212,
+        name: "Nicolas",
+        email: "nico@las.com"
+      }
+    },
+    {
+      id: 11111,
+      title: "Video perfect",
+      description: "This is something I love",
+      views: 24,
+      videoFile: "https://archive.org/details/BigBuckBunny_124",
+      creator: {
+        id: 121212,
+        name: "Nicolas",
+        email: "nico@las.com"
+      }
     }
-    );
-
-const db = mongoose.connection;
-
-const handleOpen = () => console.log("Connected to DB");
-const handleError = (error) => console.log(`Error on DB Cnnection on DB Connection:${error}`);
-db.once("open", handleOpen);
-db.on("error");
+  ];
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -7,8 +7,8 @@ const videoRouter = express.Router();
 
 videoRouter.get(routes.videos, videos);
 videoRouter.get(routes.upload, upload);
-videoRouter.get(routes.videoDetail, videoDetail);
 videoRouter.get(routes.editVideo, editProfile);
 videoRouter.get(routes.deleteVideo, deleteVideo);
+videoRouter.get(routes.videoDetail, videoDetail);
 
 export default videoRouter;
