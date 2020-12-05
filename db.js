669013e67bd8c4baf05adfc0a864d6e6b19e6b60
J-works-dev <sangjoon.jeremy.lee@gmail.com>
2020-12-05 22:12:35 +0800

    #2.21 to #2.25

--- db.js
+++ db.js
@@ -21,7 +21,7 @@ export const videos = [
       title: "Video awesome",
       description: "This is something I love",
       views: 24,
-      videoFile: "https://archive.org/details/BigBuckBunny_124",
+      videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
       creator: {
         id: 121212,
         name: "Nicolas",
@@ -33,7 +33,7 @@ export const videos = [
       title: "Video super",
       description: "This is something I love",
       views: 24,
-      videoFile: "https://archive.org/details/BigBuckBunny_124",
+      videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
       creator: {
         id: 121212,
         name: "Nicolas",
@@ -45,7 +45,7 @@ export const videos = [
       title: "Video nice",
       description: "This is something I love",
       views: 24,
-      videoFile: "https://archive.org/details/BigBuckBunny_124",
+      videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
       creator: {
         id: 121212,
         name: "Nicolas",
@@ -57,7 +57,7 @@ export const videos = [
       title: "Video perfect",
       description: "This is something I love",
       views: 24,
-      videoFile: "https://archive.org/details/BigBuckBunny_124",
+      videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
       creator: {
         id: 121212,
         name: "Nicolas",
