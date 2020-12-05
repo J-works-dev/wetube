a110e6de099a025352d96efbcdbf9f8b684e6803
J-works-dev <sangjoon.jeremy.lee@gmail.com>
2020-12-05 19:23:48 +0800

    temp id

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,10 @@ const { JSONCookie } = require("cookie-parser");
 
 export const join = (req, res) => res.render("join", { pageTitle: "Join" });
 export const login = (req, res) => res.render("login", { pageTitle: "Login" });
-export const logout = (req, res) => res.render("logout", { pageTitle: "Logout" });
+export const logout = (req, res) => {
+    // To Do: Process Log Out
+    res.redirect(routes.home);
+}
 export const users = (req, res) => res.render("users", { pageTitle: "Users" });
 export const userDetail = (req, res) => res.render("userDetail", { pageTitle: "User Detail" });
 export const editProfile = (req, res) => res.render("editProfile", { pageTitle: "Edit Profile" });
